9997a81a7d06dc519ff49e61174fa8483fddfea5
Radical <romitradical@protonmail.com>
2020-01-10 23:07:37 +0530

    added p2sh support

--- lib/src/utils/network.dart
+++ lib/src/utils/network.dart
@@ -9,20 +9,24 @@ class Network {
   static const bchPublic = 0x00;
   static const bchTestnetPublic = 0x6f;
 
+  static const bchPublicscriptHash = 0x05;
+  static const bchTestnetscriptHash = 0xc4;
+
   final int bip32Private;
   final int bip32Public;
   final bool testnet;
   final int pubKeyHash;
+  final int scriptHash;
   final int private;
   final int public;
 
   Network(this.bip32Private, this.bip32Public, this.testnet, this.pubKeyHash,
-      this.private, this.public);
+      this.scriptHash, this.private, this.public);
 
   factory Network.bitcoinCash() =>
-      Network(0x0488ade4, 0x0488b21e, false, 0x00, bchPrivate, bchPublic);
-  factory Network.bitcoinCashTest() => Network(
-      0x04358394, 0x043587cf, true, 0x6f, bchTestnetPrivate, bchTestnetPublic);
+      Network(0x0488ade4, 0x0488b21e, false, 0x00, 0x05, bchPrivate, bchPublic);
+  factory Network.bitcoinCashTest() => Network(0x04358394, 0x043587cf, true,
+      0x6f, 0xc4, bchTestnetPrivate, bchTestnetPublic);
 
   String get prefix => this.testnet ? "bchtest" : "bitcoincash";
 }
