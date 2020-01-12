e381552b006eb02a687428130683bb8f11916cc3
Radical <36259950+RomitRadical@users.noreply.github.com>
2020-01-12 15:12:54 +0530

    converted Crypto to a public class

--- lib/src/crypto/crypto.dart
+++ lib/src/crypto/crypto.dart
@@ -5,17 +5,19 @@ import "package:pointycastle/macs/hmac.dart";
 import "package:pointycastle/digests/ripemd160.dart";
 import "package:pointycastle/digests/sha256.dart";
 
-Uint8List hash160(Uint8List buffer) {
-  Uint8List _tmp = new SHA256Digest().process(buffer);
-  return new RIPEMD160Digest().process(_tmp);
-}
+class Crypto {
+  static Uint8List hash160(Uint8List buffer) {
+    Uint8List _tmp = new SHA256Digest().process(buffer);
+    return new RIPEMD160Digest().process(_tmp);
+  }
 
-Uint8List hmacSHA512(Uint8List key, Uint8List data) {
-  final _tmp = new HMac(new SHA512Digest(), 128)..init(new KeyParameter(key));
-  return _tmp.process(data);
-}
+  static Uint8List hmacSHA512(Uint8List key, Uint8List data) {
+    final _tmp = new HMac(new SHA512Digest(), 128)..init(new KeyParameter(key));
+    return _tmp.process(data);
+  }
 
-Uint8List hash256(Uint8List buffer) {
-  Uint8List _tmp = new SHA256Digest().process(buffer);
-  return new SHA256Digest().process(_tmp);
+  static Uint8List hash256(Uint8List buffer) {
+    Uint8List _tmp = new SHA256Digest().process(buffer);
+    return new SHA256Digest().process(_tmp);
+  }
 }
