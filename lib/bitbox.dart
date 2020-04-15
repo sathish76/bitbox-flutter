943eab623427c67288b9e3164c54e747adaa2b71
Radical <romitradical@protonmail.com>
2020-04-15 14:03:16 +0530

    import more files

--- lib/bitbox.dart
+++ lib/bitbox.dart
@@ -2,6 +2,7 @@ library bitbox;
 
 export 'src/account.dart';
 export 'src/address.dart';
+export 'src/bchaddress.dart';
 export 'src/bitbox.dart';
 export 'src/bitcoincash.dart';
 export 'src/block.dart';
@@ -10,6 +11,9 @@ export 'src/crypto/crypto.dart';
 export 'src/ecpair.dart';
 export 'src/hdnode.dart';
 export 'src/mnemonic.dart';
+export 'src/networks.dart';
+export 'src/privatekey.dart';
+export 'src/publickey.dart';
 export 'src/rawtransactions.dart';
 export 'src/transaction.dart';
 export 'src/transactionbuilder.dart';
