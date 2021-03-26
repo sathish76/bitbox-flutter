76b6f756701095ae03a4b50e98811990be357c26
devanshig <devanshig@geekyants.com>
2021-03-26 10:04:46 +0530

    upgraded dependencies

--- lib/src/cashaccounts.dart
+++ lib/src/cashaccounts.dart
@@ -8,20 +8,20 @@ class CashAccounts {
     if (collision != null) {
       col = collision.toString();
     }
-    final response = await http.get(
-        "https://rest.bitcoin.com/v2/cashAccounts/lookup/$account/$number/$col");
+    final response = await http.get(Uri.parse(
+        "https://rest.bitcoin.com/v2/cashAccounts/lookup/$account/$number/$col"));
     return json.decode(response.body);
   }
 
   static Future<Map> check(String account, int number) async {
-    final response = await http
-        .get("https://rest.bitcoin.com/v2/cashAccounts/check/$account/$number");
+    final response = await http.get(Uri.parse(
+        "https://rest.bitcoin.com/v2/cashAccounts/check/$account/$number"));
     return json.decode(response.body);
   }
 
   static Future<Map> reverseLookup(String cashAddress) async {
-    final response = await http.get(
-        "https://rest.bitcoin.com/v2/cashAccounts/reverseLookup/$cashAddress");
+    final response = await http.get(Uri.parse(
+        "https://rest.bitcoin.com/v2/cashAccounts/reverseLookup/$cashAddress"));
     return json.decode(response.body);
   }
 
@@ -30,7 +30,8 @@ class CashAccounts {
       'name': name,
       'payments': [address]
     };
-    final response = await http.post('https://api.cashaccount.info/register',
+    final response = await http.post(
+        Uri.parse('https://api.cashaccount.info/register'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode(register));
     Map data = jsonDecode(response.body);
