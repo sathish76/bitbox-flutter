3fee6786f83ed5d915378cbb41d289a693620da0
RomitRadical <romitradical@protonmail.com>
2022-05-18 18:36:32 +0530

    fix encoding/decoding issue with pointy castle

--- lib/src/crypto/ecurve.dart
+++ lib/src/crypto/ecurve.dart
@@ -2,7 +2,6 @@ import 'dart:typed_data';
 import 'package:hex/hex.dart';
 import 'package:pointycastle/ecc/api.dart' show ECPoint;
 import 'package:pointycastle/ecc/curves/secp256k1.dart';
-import "package:pointycastle/src/utils.dart";
 
 class ECurve {
   static final secp256k1 = new ECCurve_secp256k1();
@@ -13,6 +12,28 @@ class ECurve {
       "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
   static final ecP = HEX.decode(
       "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f");
+
+  /// Decode a BigInt from bytes in big-endian encoding.
+  static BigInt decodeBigInt(List<int> bytes) {
+    var result = BigInt.from(0);
+    for (var i = 0; i < bytes.length; i++) {
+      result += BigInt.from(bytes[bytes.length - i - 1]) << (8 * i);
+    }
+    return result;
+  }
+
+  /// Encode a BigInt into bytes using big-endian encoding.
+  static Uint8List encodeBigInt(BigInt number) {
+    var _byteMask = BigInt.from(0xff);
+    // Not handling negative numbers. Decide how you want to do that.
+    var size = (number.bitLength + 7) >> 3;
+    var result = Uint8List(size);
+    for (var i = 0; i < size; i++) {
+      result[size - i - 1] = (number & _byteMask).toInt();
+      number = number >> 8;
+    }
+    return result;
+  }
 
   static Uint8List privateAdd(Uint8List d, Uint8List tweak) {
 //    if (!isPrivate(d)) throw new ArgumentError(THROW_BAD_PRIVATE);
